13f3e76b9dc68d21c8f6244f0831c0b44b0c40ba
moath bahasan <moazbahasan@gmail.com>
2021-03-29 11:25:52 +0300

    finish button textfiles card banner

--- src/studentname.js
+++
@@ -1,1 +0,0 @@
-module.exports = (firstName, lastName) => console.log(firstName+ " "+ lastName);
--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,14 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     entry: {
-        'app': './src/index.js',
+        'app':              './src/index.js',
+        'assets/js/banner': './src/assets/js/banner.js',
     },
 
     output : {
         publicPath: '/',
         path: path.join(__dirname, "/app"),
-        filename: 'app.js',
+        filename: '[name].js',
     },
     devServer: {
         contentBase: path.join(__dirname, "/app"),
@@ -30,6 +31,16 @@ module.exports = {
                     }
                 ]
             },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              },
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
@@ -65,7 +76,32 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: "index.html",
             template: "./src/index.html",
-        })
+            chunks: ['app']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "components/button.html",
+            template: "./src/components/button.html",
+            chunks: ['app']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "components/textfield.html",
+            template: "./src/components/textfield.html",
+            chunks: ['app']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "components/card.html",
+            template: "./src/components/card.html",
+            chunks: ['app']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "components/banner.html",
+            template: "./src/components/banner.html",
+            chunks: ['app', 'assets/js/banner']
+        }),
     ]
 }
 
